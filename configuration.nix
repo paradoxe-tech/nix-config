33b53f6944ec753571ec274387c713338e1ee1c1
= <matheo@paradoxe.tech>
2024-10-08 22:55:17 +0200

    defaults

---
+++ configuration.nix
@@ -0,0 +1,88 @@
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [
+      ./hardware-configuration.nix
+    ];
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  networking.hostName = "nixos";
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+  networking.networkmanager.enable = true;
+
+  time.timeZone = "Europe/Paris";
+  i18n.defaultLocale = "fr_FR.UTF-8";
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "fr_FR.UTF-8";
+    LC_IDENTIFICATION = "fr_FR.UTF-8";
+    LC_MEASUREMENT = "fr_FR.UTF-8";
+    LC_MONETARY = "fr_FR.UTF-8";
+    LC_NAME = "fr_FR.UTF-8";
+    LC_NUMERIC = "fr_FR.UTF-8";
+    LC_PAPER = "fr_FR.UTF-8";
+    LC_TELEPHONE = "fr_FR.UTF-8";
+    LC_TIME = "fr_FR.UTF-8";
+  };
+
+  services.xserver.enable = true;
+  # services.xserver.displayManager.gdm.enable = true;
+  # services.xserver.desktopManager.gnome.enable = true;
+
+  services.xserver.displayManager.sddm.enable = true;
+  services.xserver.desktopManager.plasma5.enable = true;
+
+
+  services.xserver = {
+    layout = "fr";
+    xkbVariant = "";
+  };
+
+  console.keyMap = "fr";
+  services.printing.enable = true;
+  sound.enable = true;
+  hardware.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    # If you want to use JACK applications, uncomment this
+    #jack.enable = true;
+  };
+
+  users.users.paradoxe = {
+    isNormalUser = true;
+    description = "paradoxe";
+    extraGroups = [ "networkmanager" "wheel" ];
+  };
+  
+  nixpkgs.config.allowUnfree = true;
+
+  environment.systemPackages = with pkgs; [
+    vim
+    wget
+    (opera.override { proprietaryCodecs = true; })
+    discord
+    obsidian
+    vscodium
+    git
+    kitty
+    rstudio
+    gramps
+  ];
+  
+  nixpkgs.config.permittedInsecurePackages = [
+    "electron-25.9.0"
+  ];
+  
+  boot.extraModulePackages = with pkgs.linuxKernel.packages.linux_6_1; [
+     rtl88x2bu # driver for wlan USB
+  ];
+
+  system.stateVersion = "23.11";
+
+}
